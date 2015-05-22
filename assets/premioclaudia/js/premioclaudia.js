6ea502795db1699830a88463aeaf953681488d71
Daniel Yada <danielyada@HDU18378.local>
2015-05-22 15:03:44 -0300

    Ajustes coração

--- assets/premioclaudia/js/premioclaudia.js
+++ assets/premioclaudia/js/premioclaudia.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    $('.coracao').on('click', function(){
+    $('.candidato').on('click', function(){
         $('.coracao').removeClass('animated bounceIn');
-        $(this).css("background", "url('../assets/premioclaudia/img/coracao-vermelho.svg') no-repeat center / contain");
+        $(this).children(".coracao").css("background", "url('../assets/premioclaudia/img/coracao-vermelho.svg') no-repeat center / contain");
         $(this).addClass('animated bounceIn');
     });
 });
