91e85b93881ce666d6b1e4dd26560c635bb9e65f
Daniel Yada <danielyada@HDU18378.local>
2015-05-22 15:08:08 -0300

    S2

--- assets/premioclaudia/js/premioclaudia.js
+++ assets/premioclaudia/js/premioclaudia.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
         $('.coracao').removeClass('animated bounceIn');
         $('.coracao').css("background", "url('../assets/premioclaudia/img/coracao-preto.svg') no-repeat center / contain");
         $(this).children(".coracao").css("background", "url('../assets/premioclaudia/img/coracao-vermelho.svg') no-repeat center / contain");
-        $(this).addClass('animated bounceIn');
+        $(this).children(".coracao").addClass('animated bounceIn');
     });
 });
