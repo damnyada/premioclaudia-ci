ec1540a7baebb0cc9855be7499e07f134a3bcbbc
Daniel Yada <danielyada@HDU18378.local>
2015-05-22 15:00:08 -0300

    Ajustes na animacao

--- assets/premioclaudia/js/premioclaudia.js
+++ assets/premioclaudia/js/premioclaudia.js
@@ -1,9 +1,7 @@
-$(".candidato").on("click", function() {
-    $(".coracao").css("background", "url('../assets/premioclaudia/img/coracao-vermelho.svg')");
-});
-
 $(document).ready(function(){
     $('.coracao').on('click', function(){
+        $('.coracao').removeClass('animated bounceIn');
+        $(this).css("background", "url('../assets/premioclaudia/img/coracao-vermelho.svg')");
         $(this).addClass('animated bounceIn');
     });
 });
