28adb94a722be8445540e130d5ead22e0034c25c
Daniel Yada <danielyada@HDU18378.local>
2015-05-22 15:01:16 -0300

    Ajustes na animacao

--- assets/premioclaudia/js/premioclaudia.js
+++ assets/premioclaudia/js/premioclaudia.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.coracao').on('click', function(){
         $('.coracao').removeClass('animated bounceIn');
-        $(this).css("background", "url('../assets/premioclaudia/img/coracao-vermelho.svg')");
+        $(this).css("background", "url('../assets/premioclaudia/img/coracao-vermelho.svg') no-repeat center / contain");
         $(this).addClass('animated bounceIn');
     });
 });
