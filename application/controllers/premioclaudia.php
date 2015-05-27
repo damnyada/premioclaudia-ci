4daea6addd143f8ae92e7c8329b24dd0b397a277
Daniel Yada <danielyada@HDU18378.local>
2015-05-27 11:08:30 -0300

    Testes backend

--- application/controllers/premioclaudia.php
+++ application/controllers/premioclaudia.php
@@ -37,17 +37,19 @@ class premioclaudia extends CI_Controller {
 	public function envia(){
 
 		if($_POST['c1']){
-            //echo "YES"; die();
+            var_dump ($_POST); die();
 			$curl_data_connect = json_encode(
 			    array(
 			            'pdInitiate' => array(
 			                'partnerGUID' => '7381bf0e-95f9-f549-9228-00003062052b',
 			                "partnerUserID" => "0",
-			                "email" => 'portalmdemulher@gmail.com',
+			                "email" => 'felipe.hardt@abril.com.br',
 			                "password" => '!@K2b3Yd%8$dS@nwBCWQpfedpS',
 			            )
 			        )
 			    );
+
+//            echo $curl_data_connect; die();
 
 			$ch = curl_init();
 			curl_setopt( $ch, CURLOPT_URL, "https://api.polldaddy.com/" );
@@ -57,11 +59,13 @@ class premioclaudia extends CI_Controller {
 			curl_setopt( $ch, CURLOPT_POSTFIELDS, $curl_data_connect );
 			$data = curl_exec( $ch );
 			curl_close( $ch );
+
+//            echo ($ch); die();
 			$dt = json_decode( $data );
 
 
-//            $usercode = $P$BDx5QDV8qqVvw6Of3y692Fh0AjFU8d\/;
-			$usercode = $dt->pdResponse->userCode;
+            $usercode = '$P$BDx5QDV8qqVvw6Of3y692Fh0AjFU8d\/';
+//			$usercode = $dt->pdResponse->userCode;
 			$categorias = array("categorias", "8887099");
 
 			for ($i = 1; $i <= 1; $i++) {
