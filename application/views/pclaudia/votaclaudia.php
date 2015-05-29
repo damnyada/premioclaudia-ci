c4c6418ad76e92b00e2ed5f4ded050b599a13066
Daniel Yada <danielyada@HDU18378.local>
2015-05-29 18:29:58 -0300

    Troca de ordens CSS

--- application/views/pclaudia/votaclaudia.php
+++ application/views/pclaudia/votaclaudia.php
@@ -5,9 +5,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <link rel="stylesheet" type="text/css" href="../assets/premioclaudia/css/animate.css">
-        <link rel="stylesheet" type="text/css" href="http://static.mdemulher.abril.com.br/mobi/css/style.css">
         <link rel="stylesheet" type="text/css" href="/../assets/premioclaudia/css/mobile-menu.css">
         <link rel="stylesheet" type="text/css" href="/../assets/premioclaudia/css/votaclaudia.css">
+        <link rel="stylesheet" type="text/css" href="http://static.mdemulher.abril.com.br/mobi/css/style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="../assets/premioclaudia/js/script_menu_m.js?v=2.0"></script>
     </head>
