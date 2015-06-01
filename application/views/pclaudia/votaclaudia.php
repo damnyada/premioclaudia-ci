1a11db05d67775a1804b81f3e3420998b0d52f63
Daniel Yada <danielyada@HDU18378.local>
2015-06-01 17:33:09 -0300

    Remocao de CSS

--- application/views/pclaudia/votaclaudia.php
+++ application/views/pclaudia/votaclaudia.php
@@ -5,11 +5,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <link rel="stylesheet" type="text/css" href="/../assets/premioclaudia/css/animate.css">
-<!--        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">-->
-        <link rel="stylesheet" type="text/css" href="/../assets/premioclaudia/css/mobile-menu.css">
         <link rel="stylesheet" type="text/css" href="/../assets/premioclaudia/css/votaclaudia.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<!--        <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>-->
     </head>
     <body>
 
