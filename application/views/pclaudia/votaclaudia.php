9041a4cf4db58da7511b60b69fb0b0c5364820ed
Daniel Yada <danielyada@HDU18378.local>
2015-06-01 18:56:55 -0300

    GA

--- application/views/pclaudia/votaclaudia.php
+++ application/views/pclaudia/votaclaudia.php
@@ -7,6 +7,13 @@
         <link rel="stylesheet" type="text/css" href="/../assets/premioclaudia/css/animate.css">
         <link rel="stylesheet" type="text/css" href="/../assets/premioclaudia/css/votaclaudia.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script async="" src="//www.googletagmanager.com/gtm.js?id=GTM-5L3XPG"></script>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-5L3XPG');
+        </script>
     </head>
     <body>
 
